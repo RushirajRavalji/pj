0510e1c681e7323ca1dda36448361507ce8ad202
RushirajRavalji <113307376+RushirajRavalji@users.noreply.github.com>
2022-10-22 14:31:03 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,22 @@
+var data= [
+    {
+        "qs": "Q1. What is 2+2?",
+        "options": [
+            "5",
+            "4",
+            "9",
+            "1"
+        ],
+        "answerId": 2
+    },
+    {
+        "qs": "Q2. What is 5+5?",
+        "options": [
+            "5",
+            "4",
+            "9",
+            "10"
+        ],
+        "answerId": 4 
+    }
+]
